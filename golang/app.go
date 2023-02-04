07fa4dd9c2d7354e4a69786e241aef7d855bd8e1
Sean Connelly <sconnel42@gmail.com>
2023-02-04 13:26:41 -0700

    Add golang server

---
+++ golang/app.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+func main() {
+    r := gin.Default()
+    r.GET("/ping", func(c *gin.Context) {
+        c.JSON(http.StatusOK, gin.H{
+            "message": "pong",
+        })
+    })
+    r.Run() // http://localhost:8080
+}
