ff670d5dbeab7aac0a3dbb9fa820209079327d04
Sean Connelly <sconnel42@gmail.com>
2023-02-04 13:35:18 -0700

    Ping endpoint for Python too

--- python/api_comparer_python/main.py
+++ python/api_comparer_python/main.py
@@ -3,6 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/ping")
+async def ping():
+    return {"message": "pong"}
